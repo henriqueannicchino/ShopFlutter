4566079a951eac42b0a88cc31a21fa1e1d691e94
Luis Henrique Matos Sales <luiz.sales@cs.inf.br>
2022-10-15 01:15:54 -0400

    added filter favorites

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:shop/components/product_grid.dart';
 
+enum FilterOptions {
+  Favorite,
+  All
+}
 
-class ProductOverviewPage extends StatelessWidget {
- 
-  ProductOverviewPage({super.key});
+class ProductOverviewPage extends StatefulWidget {
+  ProductOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProductOverviewPage> createState() => _ProductOverviewPageState();
+}
+
+class _ProductOverviewPageState extends State<ProductOverviewPage> {
+  bool _showFavoriteOnly = false;
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +22,33 @@ class ProductOverviewPage extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: Text('Minha Loja'),
+        actions: [
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Somente Favoritos'),
+                value: FilterOptions.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.All,
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorite) {
+                  _showFavoriteOnly = true;
+                } else {
+                  _showFavoriteOnly = false;
+                }
+              });
+              
+            },
+          ),
+        ],
       ),
-      body: ProductGrid(),
+      body: ProductGrid(_showFavoriteOnly),
     );
   }
 }
